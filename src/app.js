bd45bebaf4134f2d7fd8eb88596b671d389585f6
SAHIL <sahilrasal742@gmail.com>
2025-08-02 17:54:01 +0530

    https methods done get,put,post,delete

--- src/app.js
+++ src/app.js
@@ -2,16 +2,21 @@ const express = require("express");
 
 const app = express();
 
-app.use("/best", (req, res) => {
-  res.send("hello best");
+app.get("/best", (req, res) => {
+  res.send("sent success using get");
 });
 
-app.use("/test", (req, res) => {
+app.post("/best", (req, res) => {
+  res.send("sent success using post");
+});
+
+app.use("/test/:username/:password", (req, res) => {
+  console.log(req.params);
   res.send("hello test");
 });
 
-app.use((req, res) => {
-  res.send("hello sssrother");
+app.use("/", (req, res) => {
+  res.send("hello brother");
 });
 
 app.listen(7777, () => {
