4f11f619f1f08dd2c3baaf2753d66b22d7bcb8da
SAHIL <sahilrasal742@gmail.com>
2025-08-06 13:59:22 +0530

    delete and update opertaions performed

--- src/app.js
+++ src/app.js
@@ -5,6 +5,48 @@ const User = require("./models/user");
 
 app.use(express.json());
 
+app.get("/user", async (req, res) => {
+  console.log(req.body.emailId);
+  const userEmail = await User.find({ emailId: req.body.emailId });
+  try {
+    res.send(userEmail);
+  } catch {
+    res.status(400).send("Something went wrong" + err.message);
+  }
+});
+
+app.get("/feed", async (req, res) => {
+  console.log(req.body);
+  const users = await User.find({});
+  try {
+    res.send(users);
+  } catch {
+    res.status(400).send("Something went wrong" + err.message);
+  }
+});
+
+app.delete("/user", async (req, res) => {
+  const user = req.body.id;
+  const userId = await User.findOneAndDelete({ _id: user });
+  try {
+    res.send("Deleted User");
+  } catch (err) {
+    res.status(400).send("Error deleting the user" + err.message);
+  }
+});
+
+app.patch("/user", async (req, res) => {
+  const user = req.body.id;
+  const data = req.body;
+  // console.log(user);
+  const userId = await User.findByIdAndUpdate({ _id: user }, data);
+  try {
+    res.send("User info updated successfully");
+  } catch {
+    res.status(400).send("Error deleting the user" + err.message);
+  }
+});
+
 app.post("/signup", async (req, res) => {
   console.log(req.body);
   const user = new User(req.body);
