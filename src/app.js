4462b16866289ba3c54d5a45dec4c0b43030431e
SAHIL <sahilrasal742@gmail.com>
2025-08-06 09:16:37 +0530

    dynamic api created

--- src/app.js
+++ src/app.js
@@ -2,13 +2,12 @@ const express = require("express");
 const connectDB = require("./config/database");
 const app = express();
 const User = require("./models/user");
+
+app.use(express.json());
+
 app.post("/signup", async (req, res) => {
-  const user = new User({
-    firstName: "Sahil",
-    lastName: "Rasal",
-    email: "sahil@gmail.com",
-    password: "sahil123",
-  });
+  console.log(req.body);
+  const user = new User(req.body);
   try {
     await user.save();
     res.send("User created");
